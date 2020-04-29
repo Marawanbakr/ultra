c35c2f0f678854271cb6fd09ad3640f7e491b09f
Ahmed Younes <cahmedc@gmail.com>
2020-04-29 05:25:56 +0200

    typo fix تصحيح خطأ كتابة

--- src/Component/Navbar/index.js
+++ src/Component/Navbar/index.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const Navbar = ()=> {
     return (
       <div>
-         <Link to="/">Home </Link>
-         <Link to="/"contact>contact</Link>
+        <Link to="/">Home </Link>
+        <Link to="/contact">contact</Link>
       </div>
-    )
+    );
 }
 export default Navbar; 
